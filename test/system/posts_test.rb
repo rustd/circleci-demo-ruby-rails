ad9223acf05a656873f463df2cb5839b96e0462e
Pranav Rastogi <rustd@users.noreply.github.com>
2022-09-14 15:00:48 -0700

    Update posts_test.rb

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -3,9 +3,9 @@
 require 'application_system_test_case'
 
 class PostsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit posts_url
-  #
-  #   assert_selector "h1", text: "Post"
-  # end
+   test "visiting the index" do
+     visit posts_url
+  
+     assert_selector "h1", text: "Post"
+   end
 end
